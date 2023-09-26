f74ec4371fb2621fde423cd414a7bc9bc8fd90c6
Julian <avflyer228@gmail.com>
2023-09-26 05:22:56 -0400

    added code files for CollabotronClient.BeatmapHandler, CollabotronClient.HTTPHandler

---
+++ client/CollabotronClient/BeatmapHandler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Editor_Reader;
+
+namespace CollabotronClient
+{
+    class BeatmapHandler
+    {
+        private string filePath;
+        private EditorReader reader;
+
+        public BeatmapHandler()
+        {
+            reader = new EditorReader();
+        }
+
+        public string GetFileName()
+        {
+            return filePath;
+        }
+
+        public void ReadEditor()
+        {
+            reader.FetchAll();
+            filePath = Path.Combine(reader.ContainingFolder, reader.Filename);
+        }
+
+        public string GetBeatmapContents()
+        {
+            return File.ReadAllText(filePath);
+        }
+
+        public void WriteToBeatmap(string data)
+        {
+            File.WriteAllText(filePath, data);
+        }
+    }
+}
---
+++ client/CollabotronClient/HTTPHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace CollabotronClient
+{
+    class HTTPHandler
+    {
+        private string url;
+        private string cookies;
+
+        public HTTPHandler(string url)
+        {
+            this.url = url;
+        }
+
+        public string Get(Dictionary<string, string> getParameters = null)
+        {// TODO
+            return "";
+        }
+    }
+}
